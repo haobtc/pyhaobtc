9e643232f88ff1784ec3cdb2802e4cb6da437976
freez91 <useyes91@gmail.com>
2016-10-26 14:16:26 +0800

    add usage demo

--- haobtc_oauth2/README.md
+++ haobtc_oauth2/README.md
@@ -3,6 +3,30 @@
 
 ## Installation
 
+```
+
+pip install haobtc-oauth2
+
+```
+
+## Usage
+
+```
+
+from pyoauth2 import Client
+
+client = Client(client_id, client_secret)
+
+authorize_url = client.auth_code.authorize_url(redirect_uri=REDIRECT_URL)
+print authorize_url
+
+token = client.auth_code.get_token(code)
+print token
+user_info = client.auth_code.get_resource()
+print user_info
+
+```
+
 ## License
 
 MIT
--- haobtc_oauth2/setup.py
+++ haobtc_oauth2/setup.py
@@ -4,10 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='haobtc_oauth2',
+    name='haobtc-oauth2',
+    version='0.0.1',
+    description='haobtc oauth2',
+    author='freeza91',
+    author_email='useyes91@gmail.com',
     keywords=('Python', 'pyoauth2', 'haobtc'),
     license='MIT License',
-    url='https://github.com/liluo/py-oauth2',
+    url='https://github.com/haobtc/pyhaobtc/tree/master/haobtc_oauth2',
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
@@ -18,4 +22,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         ],
+    zip_safe=False,
 )
